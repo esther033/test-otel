3e69d4dfe53a191fce452c52d87b6ff9c12904e4
esther033 <a90536408@gmail.com>
2026-04-02 01:51:26 +0900

    init

---
+++ src/main/java/dev/esther/otel/OtelApplication.java
@@ -0,0 +1,13 @@
+package dev.esther.otel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtelApplication.class, args);
+	}
+
+}
